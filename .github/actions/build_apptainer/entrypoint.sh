0f9d5555cca7c08314c86e05e577d01f8181e076
Andrew Hill <andrew.2.hill@cuanschutz.edu>
2026-06-10 09:43:19 -0600

    Modify build action to move into directory prior to build

--- .github/actions/build_apptainer/entrypoint.sh
+++ .github/actions/build_apptainer/entrypoint.sh
@@ -23,5 +23,12 @@ fi
 # Iterate each file and build the container.
 for DEF_FILE in "${DEF_FILES[@]}"
 do
-    echo "Building '${DEF_FILE}'..." && apptainer build "${DEF_FILE%.def}.sif" "$DEF_FILE"
+    # Get the enclosing directory for this file
+    DEF_DIR=$(dirname "$DEF_FILE")
+    # Get the basename of the file.
+    DEF_FILE_NODIR=$(basename "$DEF_FILE")
+    # Move into the enclosing directory and build .def file into a .sif container.
+    echo "Building '${DEF_FILE}'..." && \
+    cd "$DEF_DIR" && \
+    apptainer build "${DEF_FILE_NODIR%.def}.sif" "$DEF_FILE_NODIR"
 done
