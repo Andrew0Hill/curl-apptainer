394a92c3451498148cc1e95c635da8e881870d4b
Andrew Hill <andrew.2.hill@cuanschutz.edu>
2026-06-10 11:53:07 -0600

    Download containers fix

--- download_containers.sh
+++ download_containers.sh
@@ -3,4 +3,4 @@
 
 curl --output-dir CURL_Honestbroker/db --remote-name -L https://github.com/Andrew0Hill/curl-apptainer/releases/download/1.2.0/curl-honestbroker-db_1.2.0.sif && \
 curl --output-dir CURL_Honestbroker/app --remote-name -L https://github.com/Andrew0Hill/curl-apptainer/releases/download/1.2.0/curl-honestbroker_1.2.0.sif && \
-curl --output-dir CURL_Honestbroker/app --remote-name -L https://github.com/Andrew0Hill/curl-apptainer/releases/download/1.2.0/curl-site_1.2.0.sif
+curl --output-dir CURL_Site/ --remote-name -L https://github.com/Andrew0Hill/curl-apptainer/releases/download/1.2.0/curl-site_1.2.0.sif
