a74ee503fb3169c3cc8f86bf1eea93e393a851af
Andrew Hill <andrew.2.hill@cuanschutz.edu>
2026-06-10 11:41:51 -0600

    Add launch script

--- launch.sh
+++ launch.sh
@@ -2,13 +2,13 @@
 set -e
 
 # Start CURL Honestbroker DB
-(cd ./CURL_Honestbroker/db && ./launch_curl_honestbroker_db.sh) &
+cd ./CURL_Honestbroker/db && ./launch_curl_honestbroker_db.sh &
 
 # Start CURL Honestbroker
-(cd ./CURL_Honestbroker/app && ./launch_curl_honestbroker.sh) &
+cd ./CURL_Honestbroker/app && ./launch_curl_honestbroker.sh &
 
 # Start CURL Site
-(cd ./CURL_Site && ./launch_curl_site.sh) &
+cd ./CURL_Site && ./launch_curl_site.sh &
 
 # Wait for apps
 wait
